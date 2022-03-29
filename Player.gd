c10b4da64e76d52855d0e0697a64bf16bd98f0f8
Jade1724 <haruka.i1724@gmail.com>
2022-03-29 17:31:57 +1300

    Added motion control to the Player and instantiated into the scene Main

---
+++ Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody
+
+# How fast the player moves in meters per second.
+export var speed = 10
+
+var velocity = Vector3.ZERO
+
+func _physics_process(_delta):
+	# Input direction
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+		
+	# Normalize the speed when moving diagonal direction
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+		
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		velocity = move_and_slide(velocity, Vector3.UP)
