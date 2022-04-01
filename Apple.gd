fc94128d93db9d962773842e84fdc98c07376fef
Jade1724 <haruka.i1724@gmail.com>
2022-04-01 23:11:40 +1300

    Fixed a bug of Apple scene not instantiating.

---
+++ Apple.gd
@@ -0,0 +1,9 @@
+extends StaticBody
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func initialize(position):
+	translation = position
--- AppleTree.gd
+++ AppleTree.gd
@@ -1,12 +1,19 @@
 extends StaticBody
 
-export (PackedScene) var apple_scene
+onready var apple_scene = load("res://Apple.tscn")
 
 func _ready():
+#	randomize()
+	spawn_apple()
+	
+func spawn_apple():
 	var apple = apple_scene.instance()
+	print("spawn apple called")
 	
 	var apple_spawn_location = get_node("AppleTreePivot/SpawnPath/SpawnLocation")
-	apple_spawn_location.unit_offset = randf()\
+	
+#	apple_spawn_location.unit_offset = randf()
+#	var tree_location = self.transform.origin
 	
 	add_child(apple)
-	apple.initialize()
+	apple.initialize(apple_spawn_location.translation)
